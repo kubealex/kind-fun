e7fbeca6d19429884a15aa41ac0236ef76f313bc
kubealex <al.rossi87@gmail.com>
2025-06-06 13:50:44 +0200

    e2e working

--- prepare-setup.sh
+++ prepare-setup.sh
@@ -1,20 +1,14 @@
-# [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
-# chmod +x ./kind
-# sudo mv ./kind /usr/local/bin/kind
-
-# kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
-
 export VERSION=$(curl -s https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
 echo $VERSION
-kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml"
+kubectl apply -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml"
 
-kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml"
+kubectl apply -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml"
 kubectl apply -f https://raw.githubusercontent.com/kubevirt-manager/kubevirt-manager/main/kubernetes/bundled.yaml
 
 export TAG=$(curl -s -w %{redirect_url} https://github.com/kubevirt/containerized-data-importer/releases/latest)
 export VERSION=$(echo ${TAG##*/})
-kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
-kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-operator.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/$VERSION/cdi-cr.yaml
 
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
@@ -22,15 +16,11 @@ kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboar
 
 kubectl apply -f https://api.hub.tekton.dev/v1/resource/tekton/task/git-clone/0.9/raw
 
-kubectl apply -f ingress-kubevirt.yml -f ingress-tekton.yml -f rbac.yml
-kubectl apply -f task-image-build.yml -f task-image-sign.yml -f task-bootc-image-builder.yml -f task-kubevirt-vm.yml -f task-kubevirt-image.yml
-kubectl apply -f pipeline-bootc-image-builder.yml -f pipeline-deploy-vm.yml -f pipeline-image-build.yml
-
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
 
 helm upgrade --install --version=0.7.1 \
-    --namespace $FC_NAMESPACE --create-namespace \
+    --namespace flightctl --create-namespace \
     flightctl ./deploy/helm/flightctl/ \
     --set global.exposeServicesMethod=nodePort \
     --set kv.fsGroup=1001 \
@@ -41,29 +31,41 @@ helm upgrade --install --version=0.7.1 \
     --set global.nodePorts.agent=32003 \
     --set global.nodePorts.ui=32004 \
     --set global.nodePorts.keycloak=32005 \
-    --set "global.baseDomain=192.168.200.71.nip.io"
+    --set "global.baseDomain=192.168.200.165.nip.io"
 
-helm upgrade --install gitea gitea-charts/gitea \
+helm upgrade --install gitea --namespace gitea --create-namespace \
+gitea-charts/gitea \
 -n gitea \
 --set postgresql-ha.enabled=false \
 --set postgresql.enabled=true \
 --set gitea.admin.username="gitea" \
---set gitea.admin.password="redhat" \
+--set gitea.admin.password="redhat123" \
 --set gitea.config.webhook.ALLOWED_HOST_LIST="*" \
 --set gitea.config.webhook.SKIP_TLS_VERIFY=true \
---set gitea.config.server.ROOT_URL=gitea.ui:31080 \
+--set gitea.config.server.ROOT_URL=http://gitea.ui:31080 \
 --set ingress.enabled=true \
 --set "ingress.hosts[0].host=gitea.ui" \
 --set "ingress.hosts[0].paths[0].path=/" \
 --set "ingress.hosts[0].paths[0].pathType=Prefix"
 
 
-auth_token=$(curl -k -s -X POST  -u "gitea:redhat" http://gitea.ui:31080/api/v1/users/gitea/tokens -H "Content-Type: application/json" -d '{"name": "Gitea token", "scopes": ["write:repository", "write:user"]}' | jq -r '.sha1')
+cat trigger-rbac.yml trigger-template-image-build.yml trigger-template-bootc-image-builder.yml trigger-binding-image-build.yml trigger-event-listener-image-build.yml > tekton-triggers.yml
+cat task-* > tekton-tasks.yml
+cat pipeline-* > tekton-pipelines.yml
+cat ingress-* > lab-ingress.yml
+
+kubectl apply -f lab-ingress.yml
+kubectl apply -f tekton-tasks.yml
+kubectl apply -f tekton-pipelines.yml
+kubectl apply -f tekton-triggers.yml
+
+
+auth_token=$(curl -k -s -X POST  -u "gitea:redhat123" http://gitea.ui:31080/api/v1/users/gitea/tokens -H "Content-Type: application/json" -d '{"name": "Gitea token", "scopes": ["write:repository", "write:user"]}' | jq -r '.sha1')
 
 curl -k -X POST http://gitea.ui:31080/api/v1/repos/migrate \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $auth_token" \
-  -d '{"clone_addr": "https://github.com/kubealex/kind-fun.git", "repo_name": "devconf.cz-bootc"}'
+  -d '{"clone_addr": "https://github.com/kubealex/devconf.cz-bootc-keynote", "repo_name": "devconf.cz-bootc-keynote"}'
 
 
 curl -k -X POST \
@@ -80,3 +82,5 @@ curl -k -X POST \
       "http_method": "POST"
     }
   }'
+
+COSIGN_PASSWORD="" cosign generate-key-pair k8s://default/cosign-key-test
